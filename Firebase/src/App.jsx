bb95818f9fefa8ca7d2390e650397723db327a08
Basirat Hamayoon <basirathumayun@gmail.com>
2025-01-23 19:53:22 +0500

    my new branch

--- Firebase/src/App.jsx
+++ Firebase/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Basirat from './Basirat'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,8 +17,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Basirat />
       <h1>Vite + React</h1>
-      <h1>Me Basirat your Bro</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ Firebase/src/Basirat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basirat = () => {
+  return (
+    <div>
+         <h1>Hello I am Basirat</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sapiente cumque eveniet laborum ipsum! Unde assumenda eius animi tempora quaerat, quam dolore pariatur, dolorum perspiciatis qui numquam iste odio provident.</p>
+    </div>
+  )
+}
+
+export default Basirat
